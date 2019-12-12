6b2cd8a894210982c4c5f3916dc56967d0e77a00
Aleia Knight <aleiaknight@gmail.com>
2019-12-12 17:41:22 -0500

    modeling

--- contractor/space/views.py
+++ contractor/space/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 
-# Create your views here.
+
+test_data =[
+    {
+        'title':'Space Item',
+        'content':"This is the first space item",
+        'author':'aleiaknight'
+    },
+    {
+        'title':'Star Item',
+        'content':"This is the 1st star item",
+        'author':'aleiaknight'
+    }
+]
+
+
+
 def index(request):
     return render(request, 'home.html')
 
 def store_display(request):
-    return render(request, 'store_display.html')
+    context={
+        'items': test_data
+    }
+    return render(request, 'store_display.html', context)
