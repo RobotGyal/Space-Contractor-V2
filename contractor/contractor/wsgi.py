a539403e28db36ef1162ae613c169ca15bb559b4
aleia <student@users-MacBook-Pro.local>
2020-02-06 18:44:45 -0800

    heroku

--- contractor/contractor/wsgi.py
+++ contractor/contractor/wsgi.py
@@ -9,10 +9,13 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contractor.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contractor.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
 
