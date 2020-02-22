d98bd5ab806434658cd8b42b887b66f271519866
aleia <student@users-MacBook-Pro.local>
2020-02-22 13:40:38 -0800

    fixing form post for cart display

--- cart/urls.py
+++ cart/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cart_display, name= 'cart-display'),
+    path('', views.cart_home, name= 'cart-home'),
     path('add/', views.cart_add, name= 'cart-add'),
     path('cart_display/', views.cart_display, name= 'cart-display')
 
--- cart/views.py
+++ cart/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from .models import Item
 
 # Create your views here.
+def cart_home(request):
+    return render(request, 'cart/cart_display.html')
+
+
 def cart_display(request):
     context={
         'items': Item.objects.all()
     }
     return render(request, 'cart/cart_display.html', context)
-
 
 
 def cart_add(request):
