f1b246e3efd835018d88a6e67481c2da21e2f96f
aleia <student@users-MacBook-Pro.local>
2020-02-22 15:18:31 -0800

    error with displaying form submission on page

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', views.cart_home, name= 'cart-home'),
     path('add/', views.cart_add, name= 'cart-add'),
     path('cart_display/', views.cart_display, name= 'cart-display')
-
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from .models import Item
+from django.views.generic import ListView, CreateView
 
 # Create your views here.
 def cart_home(request):
     return render(request, 'cart/cart_display.html')
+
+
+def cart_add(request):
+    if request.method == 'POST':
+        if request.POST.get('name') and request.POST.get('content'):
+            item = Item.objects.all()
+            item.name = request.POST.get('name')
+            item.content = request.POST.get('content')
+            item.save()
+        return render(request, 'cart/cart_add.html')
+    else:
+        return render(request, 'cart/cart_add.html')
 
 
 def cart_display(request):
@@ -11,7 +24,3 @@ def cart_display(request):
         'items': Item.objects.all()
     }
     return render(request, 'cart/cart_display.html', context)
-
-
-def cart_add(request):
-    return render(request, 'cart/cart_add.html')
