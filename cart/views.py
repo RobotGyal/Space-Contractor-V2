045b57709e3708666c75b2058de211ebb71155a6
aleia <student@users-MacBook-Pro.local>
2020-02-22 13:04:30 -0800

    making changes to routes

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 def cart_display(request):
-    return render(request, 'cart/cart_display.html')
+    context={
+        'items': Item.objects.all()
+    }
+    return render(request, 'cart/cart_display.html', context)
 
 
 
--- space/views.py
+++ space/views.py
@@ -1,28 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Item
 
-
-# test_data =[
-#     {
-#         'title':'Space Item',
-#         'content':"This is the first space item",
-#         'author':'aleiaknight'
-#     },
-#     {
-#         'title':'Star Item',
-#         'content':"This is the 1st star item",
-#         'author':'aleiaknight'
-#     }
-# ]
 
 
 
 def index(request):
     return render(request, 'home.html')
-
-def store_display(request):
-    context={
-        'items': Item.objects.all()
-    }
-    return render(request, 'store_display.html', context)
