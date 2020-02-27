a35e564fe89fcb03e8fccb663b56820972c48e10
aleia <student@users-MacBook-Pro.local>
2020-02-26 16:29:04 -0800

    adding test

--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from cart.models import Item
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your tests here.
+# models test
+class WhateverTest(TestCase):
+
+    def create_item(self, name="only a test", content="yes, this is only a test"):
+        return Item.objects.create(name=name, content=content)
+
+    def test_item_creation(self):
+        rocket = self.create_item()
+        self.assertTrue(isinstance(rocket, Item))
+        self.assertEqual(rocket.__unicode__(), rocket.name)
